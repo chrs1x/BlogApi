1deb7f88592494558be4456043d3b97b39eb920d
Chris Onditi <chrisonditi1@gmail.com>
2025-09-07 18:21:22 +0100

    moved tests & code into same repo

---
+++ tests/BlogApi.Tests/CommentServiceTests.cs
@@ -0,0 +1,11 @@
+namespace BlogApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/BlogApi.Tests/UserServiceTests.cs
@@ -0,0 +1,108 @@
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using BlogApi.Services.UserService;
+using BlogApi.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.Tests
+{
+    public class UserServiceTests
+    {
+        private async Task<AppDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new AppDbContext(options);
+            await context.Database.EnsureCreatedAsync();
+            return context;
+        }
+
+
+        [Fact]
+        public async Task GetAllUsers_ReturnsAllUsers()
+        {
+            var context = await GetDbContext();
+            var service = new UserService(context);
+
+            context.Users.Add(new Models.User { Name = "Josh", Email = "jrudge@gmail.com" });
+            context.Users.Add(new Models.User { Name = "Stephan", Email = "sstacey@gmail.com" });
+            await context.SaveChangesAsync();
+
+            var users = await service.GetAllUsers();
+
+            Assert.Equal(2, users.Count());
+        }
+
+        [Fact]
+        public async Task GetUserById_ReturnsUser()
+        {
+            var context = await GetDbContext();
+            var service = new UserService(context);
+
+            var user = new Models.User { Name = "Chris", Email = "conditi@gmail.com" };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            var result = await service.GetUserById(user.Id);
+
+            Assert.NotNull(result);
+            Assert.Equal("Chris", result.Name);
+        }
+
+        [Fact]
+        public async Task CreateUser_SavesUser()
+        {
+            var context = await GetDbContext();
+            var service = new UserService(context);
+
+            var user = await service.CreateUser(new Models.CreateUserDto
+            {
+                Name = "Bob",
+                Email = "bsmith@gmail.com"
+            });
+
+            Assert.NotNull(user);
+            Assert.Equal("Bob", user.Name);
+            Assert.Single(context.Users);
+        }
+
+        [Fact]
+        public async Task UpdateUser_ModifiesExistingUser()
+        {
+            var context = await GetDbContext();
+            var service = new UserService(context);
+
+            var user = new Models.User { Name = "John", Email = "jevans@gmail.com" };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            var updated = await service.UpdateUser(new Models.UpdateUserDto
+            {
+                Name = "Mark",
+                Email = "mpotter@gmail.com"
+            }, user.Id);
+
+            Assert.Equal("Mark", updated.Name);
+            Assert.Equal("mpotter@gmail", updated.Email);
+        }
+
+        [Fact]
+        public async Task DeleteUser()
+        {
+            var context = await GetDbContext();
+            var service = new UserService(context);
+
+            var user = new Models.User { Name = "Raph", Email = "rcoppins@gmail.com" };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            var result = await service.DeleteUser(user.Id);
+
+            Assert.NotNull(user);
+            Assert.Empty(context.Users);
+        }
+    }
+}
